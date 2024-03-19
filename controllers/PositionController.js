f38d8e2128f0864a912a604ee996e2ad08029f02
pongnaret <147232179+pongnaret@users.noreply.github.com>
2024-03-20 00:43:56 +0700

    update670320.0043

--- controllers/PositionController.js
+++ controllers/PositionController.js
@@ -2,66 +2,66 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const Service = require('./Service')
+const Service = require("./Service");
 const PositionModel = require("../models/PositionModel");
 
-
-app.get('/position/list', Service.isLogin, async (req, res) => {
+//app.get('/position/list', Service.isLogin, async (req, res) => {
+app.get("/position/list", async (req, res) => {
   try {
-      const results = await PositionModel.findAll({
-          // where: {
-          //     userId: Service.getMemberId(req)
-          // },
-         // attributes: ['id', 'level', 'lname', 'usr'],
-          order: [['id', 'DESC']]
-      });
-      res.send({ message: 'success', results: results });
+    const results = await PositionModel.findAll({
+      // where: {
+      //     userId: Service.getMemberId(req)
+      // },
+      // attributes: ['id', 'level', 'lname', 'usr'],
+      order: [["id", "DESC"]],
+    });
+    res.send({ message: "success", results: results });
   } catch (e) {
-      res.statusCode = 500;
-      res.send({ message: e.message });
+    res.statusCode = 500;
+    res.send({ message: e.message });
   }
-})
-app.post('/position/insert', Service.isLogin, async (req, res) => {
+});
+app.post("/position/insert", Service.isLogin, async (req, res) => {
   try {
-      let payload = req.body;
-      payload.userId = Service.getMemberId(req);
+    let payload = req.body;
+    payload.userId = Service.getMemberId(req);
 
-      await PositionModel.create(payload);
-      res.send({ message: 'success' });
+    await PositionModel.create(payload);
+    res.send({ message: "success" });
   } catch (e) {
-      res.statusCode = 500;
-      res.send({ message: e.message });
+    res.statusCode = 500;
+    res.send({ message: e.message });
   }
-})
+});
 
-app.delete('/position/delete/:id', Service.isLogin, async (req, res) => {
+app.delete("/position/delete/:id", Service.isLogin, async (req, res) => {
   try {
-      await PositionModel.destroy({
-          where: {
-              id: req.params.id
-          }
-      });
-      res.send({ message: 'success' });
+    await PositionModel.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send({ message: "success" });
   } catch (e) {
-      res.statusCode = 500;
-      res.send({ message: e.message });
+    res.statusCode = 500;
+    res.send({ message: e.message });
   }
-})
-app.post('/position/edit', Service.isLogin, async (req, res) => {
+});
+app.post("/position/edit", Service.isLogin, async (req, res) => {
   try {
-      let payload = req.body;
-      payload.userId = Service.getMemberId(req);
+    let payload = req.body;
+    payload.userId = Service.getMemberId(req);
 
-      await PositionModel.update(payload, {
-          where: {
-              id: req.body.id
-          }
-      })
-      res.send({ message: 'success' });
+    await PositionModel.update(payload, {
+      where: {
+        id: req.body.id,
+      },
+    });
+    res.send({ message: "success" });
   } catch (e) {
-      res.statusCode = 500;
-      res.send({ message: e.message });
+    res.statusCode = 500;
+    res.send({ message: e.message });
   }
-})
+});
 
 module.exports = app;
