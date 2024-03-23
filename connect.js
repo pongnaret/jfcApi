e36e56e2daab34fcd4af008e58facd8e9dedaf6c
pongnaret <147232179+pongnaret@users.noreply.github.com>
2024-03-23 15:09:16 +0700

    Update670323.1509

--- connect.js
+++ connect.js
@@ -14,7 +14,7 @@ const { Sequelize } = require('sequelize');
 //     port: 3306
 // });
 
-const sequelize = new Sequelize('knrfsodpj8t17che', 'upbjid8joh6x4rzk', 'mlskdpf7l39k4i0z', {
+const sequelize = new Sequelize('knrfsodpj8t17che', 'upbjid8joh6x4rzk', 'gz1th2jzgyjapndx', {
     host: 'edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false,
